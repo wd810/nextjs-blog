162bfc9d61d65488e1a63863c5301ec95182592c
wangdian <36665764+wangdianyh@users.noreply.github.com>
2022-05-04 10:14:36 -0300

    add image

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,16 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function FirstPost() {
     return (
         <>
             <h1>My First Post</h1>
+            <Image
+                src="/images/avatar.png"
+                height={144}
+                width={144}
+                alt="Bubbles"
+            />
             <h2>
                 <Link href="/">
                     <a>Back to Home</a>
