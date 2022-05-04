e9f2f9cf5c8cd62de1ee4bd35fb8de620ee3a32c
wangdian <36665764+wangdianyh@users.noreply.github.com>
2022-05-04 10:32:00 -0300

    add layout css module

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,9 +1,22 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import Head from 'next/head'
+import Script from 'next/script'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
     return (
-        <>
+        <Layout>
+            <Head>
+                <title>First Post</title>
+            </Head>
+            <Script 
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy='lazyOnLoad'
+                onLoad={() =>
+                    consol.log(`script loaded successfully, window.FB`)
+                }
+            />
             <h1>My First Post</h1>
             <Image
                 src="/images/avatar.png"
@@ -16,6 +29,6 @@ export default function FirstPost() {
                     <a>Back to Home</a>
                 </Link>
             </h2>
-        </>
+        </Layout>
     );
 }
